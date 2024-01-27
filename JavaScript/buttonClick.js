caa66ce9f3e2f12cd042f11fdbe617123f27cab3
iOlivers <sztaba.oliwier@gmail.com>
2024-01-27 16:58:48 -0500

    Mobile Main Page Optimization Update

---
+++ JavaScript/buttonClick.js
@@ -0,0 +1,7 @@
+document.getElementById('LinkedInButton').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/iolivers/', '_blank');
+}, true);
+
+document.getElementById('GithubButton').addEventListener('click', function() {
+    window.open('https://github.com/iOlivers', '_blank');
+}, true);
