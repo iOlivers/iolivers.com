baa7434d5aaa7a8b97b11521722c1a77a5e1a9bb
iOlivers <sztaba.oliwier@gmail.com>
2024-01-28 21:16:31 -0500

    Added Skills/Project Section

--- JavaScript/buttonClick.js
+++ JavaScript/buttonClick.js
@@ -5,3 +5,30 @@ document.getElementById('LinkedInButton').addEventListener('click', function() {
 document.getElementById('GithubButton').addEventListener('click', function() {
     window.open('https://github.com/iOlivers', '_blank');
 }, true);
+
+
+let slideV = 0;
+
+const sourceImg = ["Bluetooth Gym Occupancy Tracker", "HTML/CSS Portfolio Site","Webscrapping Discord Chat Bot"];
+
+function slide(n){
+    slideV += n;
+    if (slideV >= sourceImg.length) {
+        slideV = 0; 
+    } else if (slideV < 0) {
+        slideV = sourceImg.length - 1;
+    }
+
+    document.querySelector('.carousel-img').src = "assets/Project0" + (slideV + 1) + ".png";
+    document.querySelector('.projectTitle').textContent = sourceImg[slideV];
+}
+
+document.getElementById('Next').addEventListener('click', function() {
+    slide(1);
+    console.log(slideV);
+}, true);
+
+document.getElementById('Prev').addEventListener('click', function() {
+    slide(-1);
+    console.log(slideV);
+}, true);
